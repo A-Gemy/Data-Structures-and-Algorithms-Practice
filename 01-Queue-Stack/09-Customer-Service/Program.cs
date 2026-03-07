3de90b5a235ed510c487d3435853d37a7610658c
Ahmed Gamal <ahmgy.mail@gmail.com>
2026-03-08 01:40:30 +0200

    QS-09 Customer Service Simulation using Queue

---
+++ 01-Queue-Stack/09-Customer-Service/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Customer_Service
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Initialize a queue to simulate customer arrivals
+            Queue<string> customers = new Queue<string>();
+            customers.Enqueue("Ahmed");
+            customers.Enqueue("Sara");
+            customers.Enqueue("Omar");
+            customers.Enqueue("Mona");
+
+            Console.WriteLine("Serving customers:\n");
+
+            // Serve customers in the order they arrived
+            while (customers.Count > 0)
+            {
+                string currentCustomer = customers.Dequeue();
+                Console.WriteLine($"Serving: {currentCustomer}");
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}

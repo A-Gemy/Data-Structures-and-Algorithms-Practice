a15c95b607ec730be2fb6cde2f1e9f89b64b0391
Ahmed Gamal <ahmgy.mail@gmail.com>
2026-03-08 01:46:47 +0200

    QS-10 Web Page Request Handling using Queue

---
+++ 01-Queue-Stack/10-Web-Page-Request-Handling/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Web_Page_Request_Handling
+{
+    class Program
+    {
+        static void Main()
+        {
+            Queue<string> requests = new Queue<string>();
+
+            // Web-like requests (still simple strings)
+            requests.Enqueue("GET /");
+            requests.Enqueue("GET /products");
+            requests.Enqueue("POST /login");
+            requests.Enqueue("GET /profile");
+
+
+            Console.WriteLine("Processing web requests:\n");
+
+            while (requests.Count > 0)
+            {
+                string currentRequest = requests.Dequeue();
+                Console.WriteLine($"Processed: {currentRequest}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}

1eafca64e5f8a3d5323fa431b554837b8c1f164a
Ahmed Gamal <ahmgy.mail@gmail.com>
2026-03-15 01:15:43 +0200

    QS-16 Rotate a Queue using Queue

---
+++ 01-Queue-Stack/16-Rotate-a-Queue/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Rotate_a_Queue
+{
+    class Program
+    {
+        static Queue<int> BuildQueue(int from, int to)
+        {
+            Queue<int> queue = new Queue<int>();
+
+            for (int i = from; i <= to; i++)
+            {
+                queue.Enqueue(i);
+            }
+
+            return queue;
+        }
+
+        static void RotateQueue(Queue<int> queue, int number)
+        {
+            if (queue.Count == 0)
+                return;
+
+            number %= queue.Count;
+
+            if (number == 0)
+                return;
+
+            for (int i = 0; i < number; i++)
+            {
+                int item = queue.Dequeue();
+                queue.Enqueue(item);
+            }
+        }
+
+        static void Main()
+        {
+            Queue<int> queue = BuildQueue(1, 5);
+            Console.WriteLine("Queue: " + string.Join(", ", queue));
+            int k = 2;
+
+            RotateQueue(queue, k);
+            Console.WriteLine("After rotation: " + string.Join(", ", queue));
+
+
+            Console.ReadKey();
+        }
+    }
+}

f80634783b346be9ff635210f99b8505a8be19db
Ahmed Gamal <ahmgy.mail@gmail.com>
2026-03-18 06:42:29 +0200

    QS-18 First Non-Repeating Character in a Stream using Queue and Dictionary

---
+++ 01-Queue-Stack/18-First-Non-Repeating-Character-in-a-Stream/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace First_Non_Repeating_Character_in_a_Stream
+{
+    class Program
+    {
+
+        static void PrintFirstNonRepeatingCharacters(string stream)
+        {
+            Dictionary<char, int> map = new Dictionary<char, int>();
+            Queue<char> queue = new Queue<char>();
+            List<char> result = new List<char>();
+
+            foreach (char ch in stream)
+            {
+                if (!map.ContainsKey(ch))
+                    map[ch] = 0;
+
+                map[ch]++;
+                queue.Enqueue(ch);
+
+                while (queue.Count > 0 && map[queue.Peek()] > 1)
+                {
+                    queue.Dequeue();
+                }
+
+                result.Add(queue.Count > 0 ? queue.Peek() : '-');
+            }
+
+            Console.WriteLine(string.Join(", ", result));
+        }
+
+        static void PrintFirstNonRepeatingCharacter(string stream)
+        {
+            Dictionary<char, int> map = new Dictionary<char, int>();
+
+            foreach (char ch in stream)
+            {
+                if (!map.ContainsKey(ch))
+                    map[ch] = 0;
+
+                map[ch]++;
+            }
+
+            foreach (char ch in stream)
+            {
+                if (map[ch] == 1)
+                {
+                    Console.WriteLine("First Non-Repeating Character: " + ch);
+                    return;
+                }
+            }
+
+            Console.WriteLine("No non-repeating character found.");
+        }
+
+        static void Main()
+        {
+            string input = "aabbcd";
+
+            Console.WriteLine("Input: " + input);
+
+            Console.Write("First non-repeating characters: ");
+            PrintFirstNonRepeatingCharacters(input);
+
+            PrintFirstNonRepeatingCharacter(input);
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 01-Queue-Stack/18-First-Non-Repeating-Character-in-a-Stream/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("18-First-Non-Repeating-Character-in-a-Stream")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("18-First-Non-Repeating-Character-in-a-Stream")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ae70b80e-6603-42f3-9276-cd9aea3f5829")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

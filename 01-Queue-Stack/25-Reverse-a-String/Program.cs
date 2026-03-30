3e8406d7aea6dce80403eb9481d1e0128fab5ebc
Ahmed Gamal <ahmgy.mail@gmail.com>
2026-03-31 00:51:10 +0200

    QS-25 Reverse a string using Stack

---
+++ 01-Queue-Stack/25-Reverse-a-String/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Reverse_a_String
+{
+    class Program
+    {
+        static string ReverseString(string text)
+        {
+            if (string.IsNullOrEmpty(text))
+                return text;
+
+            StringBuilder sb = new StringBuilder();
+            Stack<char> stack = new Stack<char>(text);
+
+            while (stack.Count > 0)
+            {
+                sb.Append(stack.Pop());
+            }
+
+            return sb.ToString();
+        }
+
+        static void Main()
+        {
+            string input = "Hello world";
+
+            Console.WriteLine("Input text: " + input);
+            Console.WriteLine("Reversed text: " + ReverseString(input));
+
+
+            Console.ReadKey();
+        }
+    }
+}

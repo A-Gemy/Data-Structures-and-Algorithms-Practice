0e635e543ff6149e840348e8237d0765eec53350
Ahmed Gamal <ahmgy.mail@gmail.com>
2026-03-31 00:38:54 +0200

    QS-24 Find middle element in a queue using List

---
+++ 01-Queue-Stack/24-Find-Middle-Element-in-a-Queue/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Find_Middle_Element_in_a_Queue
+{
+    class Program
+    {
+        static int FindMiddle(Queue<int> queue)
+        {
+            if (queue.Count == 0)
+                throw new InvalidOperationException("Queue is empty.");
+
+            List<int> list = new List<int>(queue);
+            return list[list.Count / 2];
+        }
+
+
+        static void Main()
+        {
+            Queue<int> queue = new Queue<int>(new[] { 1, 2, 3, 4, 5 });
+            Console.WriteLine("Middle Element: " + FindMiddle(queue));
+
+
+            Console.ReadKey();
+        }
+    }
+}

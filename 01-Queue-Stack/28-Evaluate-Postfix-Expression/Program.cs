2b8cd4859409928a6bebd628c0352ab2b18a6c26
Ahmed Gamal <ahmgy.mail@gmail.com>
2026-04-14 02:54:08 +0200

    QS-28 Evaluate postfix expression using Stack

---
+++ 01-Queue-Stack/28-Evaluate-Postfix-Expression/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Evaluate_Postfix_Expression
+{
+    internal class Program
+    {
+        private static int PerformOperation(int left, int right, char op)
+        {
+            switch (op)
+            {
+                case '+':
+                    return left + right;
+                case '-':
+                    return left - right;
+                case '*':
+                    return left * right;
+                case '/':
+                    return left / right;
+                default:
+                    throw new ArgumentException("Invalid operator.");
+            }
+        }
+
+        private static int EvaluatePostfix(string expression)
+        {
+            if (string.IsNullOrWhiteSpace(expression))
+                throw new ArgumentException("Expression cannot be empty.");
+
+            Stack<int> numbers = new Stack<int>();
+
+            foreach (char ch in expression)
+            {
+                if (char.IsWhiteSpace(ch))
+                    continue;
+
+                if (char.IsDigit(ch))
+                {
+                    int number = ch - '0';
+                    numbers.Push(number);
+                }
+                else
+                {
+                    if (numbers.Count < 2)
+                        throw new InvalidOperationException("Invalid postfix expression.");
+
+                    int right = numbers.Pop();
+                    int left = numbers.Pop();
+
+                    numbers.Push(PerformOperation(left, right, ch));
+                }
+            }
+
+            if (numbers.Count != 1)
+                throw new InvalidOperationException("Invalid postfix expression.");
+
+            return numbers.Pop();
+        }
+
+        static void Main()
+        {
+            Console.WriteLine(EvaluatePostfix("231*+9-"));
+
+            Console.ReadKey();
+        }
+
+    }
+}

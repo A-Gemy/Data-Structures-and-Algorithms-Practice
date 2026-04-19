b78ae7b78b1d115535aad8d58c4e2cfabbb2300c
Ahmed Gamal <ahmgy.mail@gmail.com>
2026-04-20 00:47:33 +0200

    DH-02 Replace tuple with value tuple for book information

--- 02-Dictionary-HashSet/02-Store-Book-Information/Program.cs
+++ 02-Dictionary-HashSet/02-Store-Book-Information/Program.cs
@@ -7,21 +7,19 @@ namespace Store_Book_Information
     {
         static void Main()
         {
-            Dictionary<string, Tuple<string, string>> books = new Dictionary<string, Tuple<string, string>>()
+            Dictionary<string, (string Title, string Author)> books = new Dictionary<string, (string, string)>
             {
-                { "978-0-7352-1129-2", Tuple.Create("Atomic Habits", "James Clear") },
-                { "978-1-61-729494-5", Tuple.Create("C# in Depth", "Jon Skeet") },
-                { "978-1-59184-644-4", Tuple.Create("The Power of Now", "Eckhart Tolle") },
-                { "978-0-135-39857-9", Tuple.Create("Clean Code", "Robert C. Martin") },
-                { "978-0-132-35088-4", Tuple.Create("1984", "George Orwell") },
+                { "978-0-7352-1129-2", ("Atomic Habits", "James Clear") },
+                { "978-1-61-729494-5", ("C# in Depth", "Jon Skeet") },
+                { "978-1-59184-644-4", ("The Power of Now", "Eckhart Tolle") },
+                { "978-0-135-39857-9", ("Clean Code", "Robert C. Martin") },
+                { "978-0-132-35088-4", ("1984", "George Orwell") },
             };
-
 
             foreach (var book in books)
             {
-                Console.WriteLine($"ISBN: {book.Key}, Title: {book.Value.Item1}, Author: {book.Value.Item2}");
+                Console.WriteLine($"ISBN: {book.Key}, Title: {book.Value.Title}, Author: {book.Value.Author}");
             }
-
 
 
             Console.ReadKey();

574c4334be280cdb0a8214a1976d069496db553d
Ahmed Gamal <ahmgy.mail@gmail.com>
2026-04-20 00:37:44 +0200

    DH-02 Store book information using Dictionary and Tuple

---
+++ 02-Dictionary-HashSet/02-Store-Book-Information/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Store_Book_Information
+{
+    class Program
+    {
+        static void Main()
+        {
+            Dictionary<string, Tuple<string, string>> books = new Dictionary<string, Tuple<string, string>>()
+            {
+                { "978-0-7352-1129-2", Tuple.Create("Atomic Habits", "James Clear") },
+                { "978-1-61-729494-5", Tuple.Create("C# in Depth", "Jon Skeet") },
+                { "978-1-59184-644-4", Tuple.Create("The Power of Now", "Eckhart Tolle") },
+                { "978-0-135-39857-9", Tuple.Create("Clean Code", "Robert C. Martin") },
+                { "978-0-132-35088-4", Tuple.Create("1984", "George Orwell") },
+            };
+
+
+            foreach (var book in books)
+            {
+                Console.WriteLine($"ISBN: {book.Key}, Title: {book.Value.Item1}, Author: {book.Value.Item2}");
+            }
+
+
+
+            Console.ReadKey();
+        }
+    }
+}

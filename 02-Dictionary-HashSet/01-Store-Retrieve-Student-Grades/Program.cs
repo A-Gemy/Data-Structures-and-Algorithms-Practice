1f4368a59224fbdc586bc424280b83b779e86d08
Ahmed Gamal <ahmgy.mail@gmail.com>
2026-04-20 00:23:33 +0200

    DH-01 Store and retrieve student grades using Dictionary

---
+++ 02-Dictionary-HashSet/01-Store-Retrieve-Student-Grades/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Store_Retrieve_Student_Grades
+{
+    class Program
+    {
+        static void Main()
+        {
+            Dictionary<string, int> studentGrades = new Dictionary<string, int>()
+            {
+                { "Alice", 85 },
+                { "Bob", 90 },
+                { "Charlie", 78 },
+                { "David", 92 }
+            };
+
+            Console.WriteLine($"Student: Bob, Grade: {studentGrades["Bob"]}");
+            Console.WriteLine();
+            foreach (var student in studentGrades)
+            {
+                Console.WriteLine($"Student: {student.Key}, Grade: {student.Value}");
+            }
+
+
+
+            Console.ReadKey();
+        }
+    }
+}

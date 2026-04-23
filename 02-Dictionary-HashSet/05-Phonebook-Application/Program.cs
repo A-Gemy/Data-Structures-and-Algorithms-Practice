4e970ea38db5029da2b86b8b96433b1944b3fed9
Ahmed Gamal <ahmgy.mail@gmail.com>
2026-04-23 21:41:18 +0200

    DH-05 Phonebook application using Dictionary

---
+++ 02-Dictionary-HashSet/05-Phonebook-Application/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Phonebook_Application
+{
+    class Program
+    {
+        static void Main()
+        {
+            Dictionary<string, string> phoneBook = new Dictionary<string, string>()
+            {
+                { "Alice", "123-456-7890" },
+                { "Bob", "987-654-3210" }
+            };
+
+            Console.WriteLine($"Alice's Phone: {phoneBook["Alice"]}");
+            Console.WriteLine($"Bob's Phone: {phoneBook["Bob"]}");
+
+
+            Console.ReadKey();
+        }
+    }
+}

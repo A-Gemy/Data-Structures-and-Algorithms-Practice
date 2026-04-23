15cd8b9488b61f18640e514156ec115a1d77c0ee
Ahmed Gamal <ahmgy.mail@gmail.com>
2026-04-23 20:53:25 +0200

    DH-04 Count word frequencies in text using Dictionary

---
+++ 02-Dictionary-HashSet/04-Count-Word-Frequencies-in-Text/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Count_Word_Frequencies_in_Text
+{
+    class Program
+    {
+        static Dictionary<string, int> CountWordFrequencies(string text, bool caseSensitive = false)
+        {
+            Dictionary<string, int> result = new Dictionary<string, int>();
+
+            if (string.IsNullOrEmpty(text))
+                return result;
+
+            string[] words = text.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (string word in words)
+            {
+                string currentWord = caseSensitive ? word : word.ToLower();
+
+                if (!result.ContainsKey(currentWord))
+                    result[currentWord] = 0;
+
+                result[currentWord]++;
+            }
+
+            return result;
+        }
+
+        static void Main()
+        {
+            string text = "Hello hello world hello universe";
+            Dictionary<string, int> result = CountWordFrequencies(text);
+
+            foreach (KeyValuePair<string, int> word in result)
+            {
+                Console.WriteLine($"{word.Key}: {word.Value}");
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}

b4d58ac6d9f2eef8951cca330832d048e26d5629
Ahmed Gamal <ahmgy.mail@gmail.com>
2026-04-23 19:40:53 +0200

    DH-03 Translate words between languages using Dictionary

---
+++ 02-Dictionary-HashSet/03-Translate-Words-Between-Languages/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Translate_Words_Between_Languages
+{
+    class Program
+    {
+        static void Main()
+        {
+            Dictionary<string, string> dictionary = new Dictionary<string, string>()
+            {
+                { "Hello", "Hola" },
+                { "World", "Mundo" },
+                { "Goodbye", "Adios" }
+            };
+
+            // Console.WriteLine($"Hello in Spanish: {dictionary["Hello"]}");
+            // Console.WriteLine($"Goodbye in Spanish: {dictionary["Goodbye"]}");
+
+            foreach (KeyValuePair<string, string> word in dictionary)
+            {
+                Console.WriteLine($"{word.Key} in Spanish: {word.Value}");
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}

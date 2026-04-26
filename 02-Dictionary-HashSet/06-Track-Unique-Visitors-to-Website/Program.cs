3c03d623a006592a2e3e79fbf0e5d8f28dd502bd
Ahmed Gamal <ahmgy.mail@gmail.com>
2026-04-27 02:53:04 +0300

    DH-06 Track unique website visitors using HashSet

---
+++ 02-Dictionary-HashSet/06-Track-Unique-Visitors-to-Website/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Track_Unique_Visitors_to_Website
+{
+    class Program
+    {
+        static void Main()
+        {
+            HashSet<string> visitors = new HashSet<string>();
+
+            visitors.Add("192.168.1.1");
+            visitors.Add("176.120.77.1");
+            visitors.Add("10.0.0.5");
+            visitors.Add("176.120.77.1");
+            visitors.Add("192.168.1.1");
+
+            Console.WriteLine($"Unique Visitors: {visitors.Count}");
+            Console.WriteLine("Visitor IPs:");
+
+            foreach (string visitor in visitors)
+            {
+                Console.WriteLine(visitor);
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}

cf7ff4b0bd2b01d6a2bef7c1bc0ed1beeac24e20
Ahmed Gamal <ahmgy.mail@gmail.com>
2026-04-27 03:05:38 +0300

    DH-07 Check duplicate entries in real time using HashSet

---
+++ 02-Dictionary-HashSet/07-Check-for-Duplicate-Entries-in-Real-Time/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Check_for_Duplicate_Entries_in_Real_Time
+{
+    class Program
+    {
+        static void Main()
+        {
+            HashSet<string> dataSet = new HashSet<string>();
+            string[] entries = { "apple", "banana", "orange", "apple", "grape", "banana" };
+
+            foreach (string item in entries)
+            {
+                if (dataSet.Add(item))
+                    Console.WriteLine($"{item} added successfully.");
+                else
+                    Console.WriteLine($"{item} is a duplicate.");
+            }
+
+            Console.WriteLine("\nFinal Unique Data:");
+            foreach (string item in dataSet)
+            {
+                Console.WriteLine(item);
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}

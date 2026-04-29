b005bb1d15c3b0239503e23ac91edc78e4a8baf1
Ahmed Gamal <ahmgy.mail@gmail.com>
2026-04-29 20:20:00 +0300

    DH-08 Dynamic skill matching using HashSet

---
+++ 02-Dictionary-HashSet/08-Dynamic-Skill-Matching/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dynamic_Skill_Matching
+{
+    class Program
+    {
+        static void Main()
+        {
+            HashSet<string> candidateSkills = new HashSet<string> { "C#", "SQL", "JavaScript" };
+            HashSet<string> jobRequirements = new HashSet<string> { "C#", "JavaScript", "React" };
+
+            HashSet<string> matchingSkills = new HashSet<string>(candidateSkills);
+            matchingSkills.IntersectWith(jobRequirements);
+
+            Console.WriteLine($"Matching Skills: " + string.Join(", ", matchingSkills));
+
+
+            Console.ReadKey();
+        }
+    }
+}

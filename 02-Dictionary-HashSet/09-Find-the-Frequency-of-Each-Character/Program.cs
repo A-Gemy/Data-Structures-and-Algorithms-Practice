15423a170a8d1264af7c815bc8f2a67649a3d4f9
Ahmed Gamal <ahmgy.mail@gmail.com>
2026-04-29 20:28:49 +0300

    DH-09 Find character frequencies using Dictionary

---
+++ 02-Dictionary-HashSet/09-Find-the-Frequency-of-Each-Character/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Find_the_Frequency_of_Each_Character
+{
+    class Program
+    {
+        private static Dictionary<char, int> CountCharacterFrequency(string text)
+        {
+            Dictionary<char, int> result = new Dictionary<char, int>();
+
+            if (string.IsNullOrEmpty(text))
+                return result;
+
+            foreach (char ch in text)
+            {
+                if (!result.ContainsKey(ch))
+                    result[ch] = 0;
+
+                result[ch]++;
+            }
+
+            return result;
+        }
+
+        static void Main()
+        {
+            string text = "hello";
+            Dictionary<char, int> result = CountCharacterFrequency(text);
+
+            foreach (KeyValuePair<char, int> item in result)
+            {
+                Console.WriteLine($"{item.Key}: {item.Value}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}

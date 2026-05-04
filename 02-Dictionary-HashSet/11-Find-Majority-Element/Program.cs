f5c1bbe74f87f59d61abb85db79601795e004df5
Ahmed Gamal <ahmgy.mail@gmail.com>
2026-05-05 02:44:16 +0300

    DH-11 Find majority element using Dictionary

---
+++ 02-Dictionary-HashSet/11-Find-Majority-Element/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Find_Majority_Element
+{
+    class Program
+    {
+        private static int FindMajorityElement(int[] numbers)
+        {
+            if (numbers == null || numbers.Length == 0)
+                return -1;
+
+            Dictionary<int, int> counts = new Dictionary<int, int>();
+
+            foreach (int number in numbers)
+            {
+                if (!counts.ContainsKey(number))
+                    counts[number] = 0;
+
+                counts[number]++;
+
+                if (counts[number] > numbers.Length / 2)
+                    return number;
+            }
+
+            return -1;
+        }
+
+        static void Main()
+        {
+            int[] numbers = { 3, 2, 3 };
+
+            Console.WriteLine($"Majority Element: {FindMajorityElement(numbers)}");
+
+
+            Console.ReadKey();
+        }
+    }
+}

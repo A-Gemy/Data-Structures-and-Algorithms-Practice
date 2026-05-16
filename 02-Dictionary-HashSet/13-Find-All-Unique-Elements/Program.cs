108b573aedf7f47cd2adcbc3e86b7726b3254ac4
Ahmed Gamal <ahmgy.mail@gmail.com>
2026-05-16 23:18:25 +0300

    DH-13 Find all unique elements using Dictionary

---
+++ 02-Dictionary-HashSet/13-Find-All-Unique-Elements/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Find_All_Unique_Elements
+{
+    class Program
+    {
+        static List<int> FindUniqueElements(int[] numbers)
+        {
+            List<int> uniqueElements = new List<int>();
+
+            if (numbers == null || numbers.Length == 0)
+                return uniqueElements;
+
+            Dictionary<int, int> counts = new Dictionary<int, int>();
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (!counts.ContainsKey(numbers[i]))
+                    counts[numbers[i]] = 0;
+
+                counts[numbers[i]]++;
+            }
+
+            foreach (KeyValuePair<int, int> pair in counts)
+            {
+                if (pair.Value == 1)
+                    uniqueElements.Add(pair.Key);
+            }
+
+            return uniqueElements;
+        }
+
+        static void Main()
+        {
+            int[] numbers = { 1, 2, 2, 3, 4, 5, 3 };
+
+            List<int> uniqueElements = FindUniqueElements(numbers);
+            Console.WriteLine("Unique Elements: " + string.Join(", ", uniqueElements));
+
+
+            Console.ReadKey();
+        }
+
+    }
+}

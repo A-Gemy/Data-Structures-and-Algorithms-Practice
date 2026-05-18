47771244c5066b52e0ee0021ad236e00d3350dac
Ahmed Gamal <ahmgy.mail@gmail.com>
2026-05-18 19:46:21 +0300

    DH-15 Find missing number in array using Dictionary

---
+++ 02-Dictionary-HashSet/15-Find-Missing-Number-in-Array/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Find_Missing_Number_in_Array
+{
+    class Program
+    {
+        private static int FindMissingNumber(int[] numbers)
+        {
+            if (numbers == null)
+                return -1;
+
+            Dictionary<int, bool> numberMap = new Dictionary<int, bool>();
+
+            int n = numbers.Length;
+            for (int i = 0; i < n; i++)
+            {
+                if (!numberMap.ContainsKey(numbers[i]))
+                    numberMap[numbers[i]] = true;
+            }
+
+            for (int i = 0; i <= n; i++)
+            {
+                if (!numberMap.ContainsKey(i))
+                    return i;
+            }
+
+            return -1; // No missing number found
+        }
+
+        static void Main()
+        {
+            int[] numbers = { 3, 0, 1 };
+
+            Console.WriteLine("Numbers in array: " + string.Join(", ", numbers));
+            Console.WriteLine("Missing number: " + FindMissingNumber(numbers));
+
+
+            Console.ReadKey();
+        }
+    }
+}

c95116268d186ab3c9a9c84205fcb87a5381ef7d
Ahmed Gamal <ahmgy.mail@gmail.com>
2026-06-22 01:12:50 +0300

    DH-19 Check if two arrays are disjoint using HashSet

---
+++ 02-Dictionary-HashSet/19-Check-if-Two-Arrays-are-Disjoint/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Check_if_Two_Arrays_are_Disjoint
+{
+    class Program
+    {
+        private static bool AreArraysDisjoint(int[] nums1, int[] nums2)
+        {
+            if (nums1 == null || nums1.Length == 0 || nums2 == null || nums2.Length == 0)
+                return true;
+
+            int[] smallerArray = nums1.Length <= nums2.Length ? nums1 : nums2;
+            int[] largerArray = nums1.Length <= nums2.Length ? nums2 : nums1;
+
+            HashSet<int> numbersSet = new HashSet<int>(smallerArray);
+
+            foreach (int num in largerArray)
+            {
+                if (numbersSet.Contains(num))
+                    return false;
+            }
+
+            return true;
+        }
+
+        static void Main()
+        {
+            int[] nums1 = { 1, 2, 3 };
+            int[] nums2 = { 4, 5, 6 };
+
+            if (AreArraysDisjoint(nums1, nums2))
+                Console.WriteLine("Arrays are disjoint.");
+            else
+                Console.WriteLine("Arrays are not disjoint.");
+
+
+            Console.ReadKey();
+        }
+    }
+}

7dd4b9ddfc894063ba761cd52b81a4cd041deb0a
Ahmed Gamal <ahmgy.mail@gmail.com>
2026-07-11 21:21:47 +0300

    SL-03 Find union of two SortedSets

---
+++ 03-SortedList/03-Union-of-Two-SortedSets/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Union_of_Two_SortedSets
+{
+    class Program
+    {
+        private static SortedSet<int> UnionOfTwoSortedSets(SortedSet<int> set1, SortedSet<int> set2)
+        {
+            if (set1 == null && set2 == null)
+                return new SortedSet<int>();
+
+            if (set1 == null)
+                return new SortedSet<int>(set2);
+
+            if (set2 == null)
+                return new SortedSet<int>(set1);
+
+            SortedSet<int> unionSet = new SortedSet<int>(set1);
+            unionSet.UnionWith(set2);
+             
+            return unionSet;
+        }
+
+        static void Main()
+        {
+            SortedSet<int> nums1 = new SortedSet<int> { 1, 2, 3 };
+            SortedSet<int> nums2 = new SortedSet<int> { 3, 4, 5 };
+
+            SortedSet<int> result = UnionOfTwoSortedSets(nums1, nums2);
+            Console.WriteLine("Union of the two sorted sets: " + string.Join(", ", result));
+
+
+            Console.ReadKey();
+        }
+    }
+}

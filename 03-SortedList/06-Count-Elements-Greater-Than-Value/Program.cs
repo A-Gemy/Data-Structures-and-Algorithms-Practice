0eb4b037280a81395f130d3fbd1192c286fcb9ed
Ahmed Gamal <ahmgy.mail@gmail.com>
2026-07-13 23:30:20 +0300

    SL-06 Count elements greater than value using SortedSet

---
+++ 03-SortedList/06-Count-Elements-Greater-Than-Value/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Count_Elements_Greater_Than_Value
+{
+    class Program
+    {
+        private static int CountElementsGreaterThanValue(SortedSet<int> set, int value)
+        {
+            if (set == null || set.Count == 0)
+                return 0;
+
+            if (value < set.Min)
+                return set.Count;
+
+            if (value >= set.Max)
+                return 0;
+
+            return set.GetViewBetween(value + 1, set.Max).Count;
+        }
+
+        static void Main()
+        {
+            SortedSet<int> set = new SortedSet<int> { 1, 2, 3, 4, 5 };
+
+            int count = CountElementsGreaterThanValue(set, 3);
+
+            Console.WriteLine("Original set: " + string.Join(", ", set));
+            Console.WriteLine("Number of elements greater than 3: " + count);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 03-SortedList/06-Count-Elements-Greater-Than-Value/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("06-Count-Elements-Greater-Than-Value")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("06-Count-Elements-Greater-Than-Value")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f0a2d9d1-6e44-4587-be20-479e9894e8c6")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

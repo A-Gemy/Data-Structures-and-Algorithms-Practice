9067bb749e89d8e9eb8967b408d7b670ee80b059
Ahmed Gamal <ahmgy.mail@gmail.com>
2026-07-15 02:23:48 +0300

    SL-08 Find elements not in range using SortedSet

---
+++ 03-SortedList/08-Find-All-Elements-Not-in-Range/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Find_All_Elements_Not_in_Range
+{
+    class Program
+    {
+        private static SortedSet<int> ElementsNotInRange(SortedSet<int> set, int low, int high)
+        {
+            if (set == null || set.Count == 0)
+                return new SortedSet<int>();
+
+            if (low > high)
+            {
+                return new SortedSet<int>(set);
+            }
+
+            SortedSet<int> result = new SortedSet<int>(set);
+            result.GetViewBetween(low, high).Clear();
+
+            return result;
+        }
+
+        static void Main()
+        {
+            SortedSet<int> set = new SortedSet<int> { 1, 2, 3, 4, 5 };
+
+            Console.WriteLine("Original set: " + string.Join(", ", set));
+
+            var result = ElementsNotInRange(set, 2, 4);
+
+            Console.WriteLine("Elements not in range [2, 4]: " + string.Join(", ", result));
+
+
+            Console.ReadKey();
+        }
+    }
+}

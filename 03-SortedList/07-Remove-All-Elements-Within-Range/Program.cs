e99894976655f7602cf0ae0adf2e24841dbcd39e
Ahmed Gamal <ahmgy.mail@gmail.com>
2026-07-14 23:03:26 +0300

    SL-07 Remove elements within range using SortedSet

---
+++ 03-SortedList/07-Remove-All-Elements-Within-Range/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Remove_All_Elements_Within_Range
+{
+    class Program
+    {
+        private static void RemoveElementsWithinRange(SortedSet<int> set, int low, int high)
+        {
+            if (set == null || set.Count == 0)
+                return;
+
+            if (low > high)
+            {
+                Console.WriteLine("Low value cannot be greater than high value.");
+                return;
+            }
+
+            set.GetViewBetween(low, high).Clear();
+        }
+
+        static void Main()
+        {
+            SortedSet<int> set = new SortedSet<int> { 1, 2, 3, 4, 5 };
+
+            Console.WriteLine("Original set: " + string.Join(", ", set));
+
+            RemoveElementsWithinRange(set, 2, 4);
+
+            Console.WriteLine("After removal: " + string.Join(", ", set));
+
+
+            Console.ReadKey();
+        }
+    }
+}

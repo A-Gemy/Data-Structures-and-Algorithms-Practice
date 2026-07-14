009c2424d72f5ab3cd707d009281aad85a8a6a11
Ahmed Gamal <ahmgy.mail@gmail.com>
2026-07-15 02:32:28 +0300

    SL-09 Count elements less than or equal to value using SortedSet

---
+++ 03-SortedList/09-Count-of-Elements-Less-Than-or-Equal-to-Value/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Count_of_Elements_Less_Than_or_Equal_to_Value
+{
+    class Program
+    {
+        private static int CountElementsLessThanOrEqualToValue(SortedSet<int> set, int value)
+        {
+            if (set == null || set.Count == 0)
+                return 0;
+
+            if (value < set.Min)
+                return 0;
+
+            if (value >= set.Max)
+                return set.Count;
+
+            return set.GetViewBetween(set.Min, value).Count;
+        }
+
+        static void Main()
+        {
+            SortedSet<int> set = new SortedSet<int> { 1, 2, 3, 4, 5 };
+
+            int count = CountElementsLessThanOrEqualToValue(set, 3);
+
+            Console.WriteLine("Original set: " + string.Join(", ", set));
+            Console.WriteLine("Number of elements less than or equal to 3: " + count);
+
+            Console.ReadKey();
+        }
+    }
+}

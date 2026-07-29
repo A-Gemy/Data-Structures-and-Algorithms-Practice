cfe8b44be0d2dc65f2247f35168bf3175496992e
Ahmed Gamal <ahmgy.mail@gmail.com>
2026-07-29 21:28:03 +0300

    OC-07 Display real-time notifications using ObservableCollection

---
+++ 04-ObservableCollection/07-Real-Time-Notification-System/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+
+namespace Real_Time_Notification_System
+{
+    class Program
+    {
+        private static void Notifications_CollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            if (e.Action == NotifyCollectionChangedAction.Add)
+            {
+                Console.WriteLine($"New notification: {e.NewItems[0]}");
+            }
+        }
+
+        static void Main()
+        {
+            ObservableCollection<string> notifications = new ObservableCollection<string>();
+
+            notifications.CollectionChanged += Notifications_CollectionChanged;
+
+            notifications.Add("Your order has been placed.");
+            notifications.Add("Your order is being prepared.");
+            notifications.Add("Your order is out for delivery.");
+            notifications.Add("Your order has been delivered.");
+
+
+            Console.ReadKey();
+        }
+    }
+}

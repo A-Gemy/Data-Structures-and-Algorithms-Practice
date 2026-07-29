1e6281b147e3ffd59bdb0474d0913e9b48ef14cb
Ahmed Gamal <ahmgy.mail@gmail.com>
2026-07-29 21:19:00 +0300

    OC-05 Display live chat messages using ObservableCollection

---
+++ 04-ObservableCollection/05-Live-Chat-Application/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+
+namespace Live_Chat_Application
+{
+    class Program
+    {
+        private static void Messages_CollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            if (e.Action == NotifyCollectionChangedAction.Add)
+            {
+                Console.WriteLine($"New message received: {e.NewItems[0]}");
+            }
+        }
+
+        static void Main()
+        {
+            ObservableCollection<string> messages = new ObservableCollection<string>();
+
+            messages.CollectionChanged += Messages_CollectionChanged;
+
+            messages.Add("Ahmed: Hello everyone!");
+            messages.Add("Sara: Hi Ahmed!");
+            messages.Add("Omar: Welcome to the chat.");
+
+            Console.ReadKey();
+        }
+    }
+}

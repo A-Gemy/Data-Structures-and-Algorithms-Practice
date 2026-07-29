7062bb306a86d7c7b8d3063caa4932a850fe7ffb
Ahmed Gamal <ahmgy.mail@gmail.com>
2026-07-29 21:24:39 +0300

    OC-06 Display dynamic weather updates using ObservableCollection

---
+++ 04-ObservableCollection/06-Dynamic-Weather-Updates/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+
+namespace Dynamic_Weather_Updates
+{
+    class Program
+    {
+        private static void WeatherUpdates_CollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            if (e.Action == NotifyCollectionChangedAction.Add)
+            {
+                Console.WriteLine($"Weather update: {e.NewItems[0]}");
+            }
+        }
+        static void Main()
+        {
+            ObservableCollection<string> weatherUpdates = new ObservableCollection<string>();
+
+            weatherUpdates.CollectionChanged += WeatherUpdates_CollectionChanged;
+
+            weatherUpdates.Add("Cairo: 32°C Sunny");
+            weatherUpdates.Add("Alexandria: 28°C Cloudy");
+            weatherUpdates.Add("New York: 25°C Sunny");
+            weatherUpdates.Add("London: 15°C Rainy");
+            weatherUpdates.Add("Mansoura: 30°C Clear");
+
+
+            Console.ReadKey();
+        }
+    }
+}

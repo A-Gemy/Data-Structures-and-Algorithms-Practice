3ff6ac0e6cffa0c671b27b3c0e63f6cceb1d20f8
Ahmed Gamal <ahmgy.mail@gmail.com>
2026-07-29 21:13:33 +0300

    OC-04 Manage tasks using ObservableCollection

---
+++ 04-ObservableCollection/04-Task-Management-System/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+
+namespace Task_Management_System
+{
+    class Program
+    {
+        private static void Tasks_CollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            switch (e.Action)
+            {
+                case NotifyCollectionChangedAction.Add:
+                    Console.WriteLine($"Task added: {e.NewItems[0]}");
+                    break;
+
+                case NotifyCollectionChangedAction.Remove:
+                    Console.WriteLine($"Task removed: {e.OldItems[0]}");
+                    break;
+
+                case NotifyCollectionChangedAction.Replace:
+                    Console.WriteLine($"Task status updated: {e.OldItems[0]} -> {e.NewItems[0]}");
+                    break;
+
+                case NotifyCollectionChangedAction.Move:
+                    Console.WriteLine($"Task moved: {e.OldItems[0]} from index {e.OldStartingIndex} to {e.NewStartingIndex}");
+                    break;
+
+                case NotifyCollectionChangedAction.Reset:
+                    Console.WriteLine("Task list cleared.");
+                    break;
+            }
+        }
+
+        static void Main()
+        {
+            ObservableCollection<string> tasks = new ObservableCollection<string>();
+
+            tasks.CollectionChanged += Tasks_CollectionChanged;
+
+            tasks.Add("Study C# - Pending");
+            tasks.Add("Finish homework - Pending");
+            tasks.Add("Practice ObservableCollection - Pending");
+
+            tasks.Remove("Study C# - Pending");
+
+            tasks[0] = "Finish homework - Completed";
+
+            tasks.Move(1, 0);
+
+            Console.WriteLine();
+            Console.WriteLine("Current Tasks:");
+
+            foreach (string task in tasks)
+            {
+                Console.WriteLine(task);
+            }
+
+            Console.WriteLine();
+
+            tasks.Clear();
+
+
+            Console.ReadKey();
+        }
+    }
+}

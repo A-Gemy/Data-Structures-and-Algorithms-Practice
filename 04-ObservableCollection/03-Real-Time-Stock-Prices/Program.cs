0b7a3c92da3874affcdd27f1f466583d92268c9a
Ahmed Gamal <ahmgy.mail@gmail.com>
2026-07-29 21:03:34 +0300

    OC-03 Track real-time stock prices using ObservableCollection

---
+++ 04-ObservableCollection/03-Real-Time-Stock-Prices/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+
+namespace Real_Time_Stock_Prices
+{
+    class Program
+    {
+        private static void StockPrices_CollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            if (e.Action == NotifyCollectionChangedAction.Add)
+            {
+                Console.WriteLine($"New Stock Price Added: {e.NewItems[0]}");
+            }
+        }
+
+        static void Main()
+        {
+            ObservableCollection<string> stockPrices = new ObservableCollection<string>();
+
+            stockPrices.CollectionChanged += StockPrices_CollectionChanged;
+
+            stockPrices.Add("AAPL: 150.00");
+            stockPrices.Add("MSFT: 240.50");
+            stockPrices.Add("GOOGL: 2800.75");
+
+
+            Console.ReadKey();
+        }
+    }
+}

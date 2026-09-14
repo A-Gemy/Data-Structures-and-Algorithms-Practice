6a33489e82792ffa0ae461ddccf0b5caaf50269d
Ahmed Gamal <ahmgy.mail@gmail.com>
2026-09-14 22:55:38 +0300

    TP-05 Represent address using ValueTuple

---
+++ 06-Tuples/05-Simplified-Address-Representation/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Simplified_Address_Representation
+{
+    class Program
+    {
+        private static (string street, string city, string state, string zipCode) GetAddress()
+        {
+            return ("123 Main St", "Springfield", "IL", "62704");
+        }
+
+        static void Main()
+        {
+            var address = GetAddress();
+
+            Console.WriteLine($"Address: {address.street}, {address.city}, {address.state}, {address.zipCode}");
+
+
+            Console.ReadKey();
+        }
+    }
+}

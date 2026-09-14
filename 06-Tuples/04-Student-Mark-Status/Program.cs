dae3ca75597a2d68d91a3c3f8b773a694481c17a
Ahmed Gamal <ahmgy.mail@gmail.com>
2026-09-14 22:47:22 +0300

    TP-04 Return student mark status using ValueTuple

---
+++ 06-Tuples/04-Student-Mark-Status/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Student_Mark_Status
+{
+    class Program
+    {
+        private static (bool isSuccess, int mark) CheckStudentMark(int mark)
+        {
+            bool isSuccess = mark >= 50;
+
+            return (isSuccess, mark);
+        }
+
+        static void Main()
+        {
+            var result = CheckStudentMark(75);
+
+            Console.WriteLine($"Success: {result.isSuccess}");
+            Console.WriteLine($"Mark: {result.mark}");
+
+
+            Console.ReadKey();
+        }
+    }
+}

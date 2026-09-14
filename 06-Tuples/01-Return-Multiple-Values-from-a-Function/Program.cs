35c88ad21dfbf18601d7f95a05c6cd0a949188fb
Ahmed Gamal <ahmgy.mail@gmail.com>
2026-09-14 22:01:46 +0300

    TP-01 Return student info using ValueTuple

---
+++ 06-Tuples/01-Return-Multiple-Values-from-a-Function/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Return_Multiple_Values_from_a_Function
+{
+    class Program
+    {
+        private static (string name, int age, double grade) GetStudentInfo()
+        {
+            return ("Ahmed", 22, 95.5);
+        }
+
+        static void Main()
+        {
+            var student = GetStudentInfo();
+            
+            Console.WriteLine($"Name: {student.name}");
+            Console.WriteLine($"Age: {student.age}");
+            Console.WriteLine($"Grade: {student.grade}");
+
+
+            Console.ReadKey();
+        }
+    }
+}

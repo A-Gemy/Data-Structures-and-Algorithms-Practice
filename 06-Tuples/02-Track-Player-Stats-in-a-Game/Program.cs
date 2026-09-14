5804a12fe93663e680127776407708c652fd29cd
Ahmed Gamal <ahmgy.mail@gmail.com>
2026-09-14 22:09:36 +0300

    TP-02 Track player stats using ValueTuple

---
+++ 06-Tuples/02-Track-Player-Stats-in-a-Game/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Track_Player_Stats_in_a_Game
+{
+    class Program
+    {
+        private static (string name, int health, int score) GetPlayerStats()
+        {
+            return ("Player1", 100, 2000);
+        }
+
+        static void Main()
+        {
+            (string name, int health, int score) player = GetPlayerStats();
+
+            Console.WriteLine($"Player Name: {player.name}");
+            Console.WriteLine($"Health: {player.health}");
+            Console.WriteLine($"Score: {player.score}");
+
+
+            Console.ReadKey();
+        }
+    }
+}
